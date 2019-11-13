e4d1f9d463497813b60f4efe59639d15b07c1e92
Patrick N <pnetsmail-ms@yahoo.com>
2019-11-13 10:05:43 +0100

    Initial commit of changes, doesn't work when using ignoreCache:true. Forces page reload.

--- 02-Calling-an-API/src/app/pages/home/home.component.ts
+++ 02-Calling-an-API/src/app/pages/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
+import { JwtHelperService } from "@auth0/angular-jwt";
+import {concatMap} from "rxjs/operators";
+import Auth0Client from "@auth0/auth0-spa-js/dist/typings/Auth0Client";
+import {from, Observable} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -7,10 +11,23 @@ import { AuthService } from 'src/app/auth/auth.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  token: string;
+  decodedToken: any;
+  jwtHelper = new JwtHelperService();
 
   constructor(public auth: AuthService) {}
 
   ngOnInit() {
+  }
+
+  async updateProfile() {
+    try {
+      this.token =  await this.auth.getTokenSilently$({ignoreCache: true}).toPromise();
+      // this.token =  await this.auth.getTokenSilently$().toPromise();
+      this.decodedToken = this.jwtHelper.decodeToken(this.token);
+    } catch (e) {
+      alert( JSON.stringify(e));
+    }
   }
 
 }
